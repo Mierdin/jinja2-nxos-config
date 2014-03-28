6a08e041e595f1424a4ef527af22d2b5efa9fb20
Matt Oswalt <matt@keepingitclassless.net>
2014-03-28 03:03:38 -0400

    Made some additions to the nexus5548 template, added a method to retrieve WWPNs for zoning configuration from Cisco UCS directly

--- main.py
+++ main.py
@@ -4,10 +4,15 @@
 	#No Fibre Channel ports configured (intent is all FCoE)
 	#Tested NXOS version TBD
 
+#Requires Cisco UCS Python SDK 0.8 to pull vHBA information
+#Tested with UCS Platform Emulator 2.2(1bPE1)
+from UcsSdk import *
+
 from jinja2 import Template, Environment, FileSystemLoader
 import csv
 
-#I'm building classes for the physical and virtual interfaces because it's fairly easy to satisfy my preconceptions from other languages, but I may revisit and simplify
+
+#Building these classes to keep things straight in my head - these may go away for simpler alternatives in the future.
 
 class PhysicalInterface:
     '''A class to define a physical interface'''
@@ -21,9 +26,9 @@ class PhysicalInterface:
         self.accessVlan = ''
         self.allowedVlans = ''
 
-
 def getFeatures():
-	# Some feature enablement/installation varies by syntax, so "features" should be an array of actual NXOS commmands to install or enable a feature #
+	# Some feature enablement/installation varies by syntax, so "features" should be an array of actual NXOS commmands to install or enable a feature 
+	# This may change in the future with some kind of "feature" class....need to think about this
 	featureArray = ['feature fcoe',
 	'install feature-set fabricpath',
 	'feature-set fabricpath',
@@ -45,7 +50,7 @@ def getVlans():
 def getVsans():
 	vsans = {321: 'TEST-VSAN-321'}
 	return vsans
-
+	
 def getTargets():
 	targets = {}
 	targets['50:00:00:00:00:11:a0:01'] = 'Netapp-01-0a' 
@@ -64,7 +69,8 @@ def getPhysicalInterfaces():
 
     pintArray = []
 
-    with open('/home/mierdin/Dropbox/Code/git/jinja2-nxos-config/Templates/n5k/physicalints.csv', 'rb') as csvfile:
+    with open('./Templates/n5k/physicalints.csv', 'rb') as csvfile:
+
         reader = csv.reader(csvfile,delimiter=';')
 
         for row in reader:
@@ -79,10 +85,35 @@ def getPhysicalInterfaces():
             thisPint.allowedVlans = row[7]
             pintArray.append(thisPint)
 
-	#print len(reader)
-	print len(pintArray)
-
 	return pintArray
+
+
+def getUcsWWPNs():
+	handle = UcsHandle()
+	handle.Login('10.12.0.136', username="config", password="config")
+
+	vHBADict = {}
+
+	#TODO: Statically defining sub-organization for now, will make more dynamic later
+	obj = handle.GetManagedObject(None, None, {"Dn":"org-root/org-ORG_TEST/"})
+	moArr = handle.GetManagedObject(obj, "vnicFc")
+	for mo in moArr:
+		#Pull only actual vHBAs (not templates) and on the desired fabric (A/B)
+		if str(mo.Addr) != 'derived' and mo.SwitchId == 'A':
+
+			#We're retrieving Dn here so we can include the service profile in the name
+			origDn = str(mo.Dn)
+			
+			#Need to do a little string surgery to transform the Dn of the vHBA into a proper zone name.
+			origDn = origDn.replace('org-root/org-ORG_TEST/','')
+			origDn = origDn.replace('/','_')
+			origDn = origDn.replace('ls-','')
+			origDn = origDn.replace('fc-','')
+
+			#using the WWPN address as key since more likely to be unique
+			vHBADict[mo.Addr] = origDn
+
+	return vHBADict
 
 env = Environment(loader=FileSystemLoader('./Templates/n5k/'))
 template = env.get_template('nexus5548UP')
@@ -91,6 +122,12 @@ print template.render(hostname='N5K-A',
 	features=getFeatures(),
 	vlanDict=getVlans(),
 	vsanDict=getVsans(),
-	initDict=getInitiators(),
+	initDict=getUcsWWPNs(),
 	targetDict=getTargets(),
 	pints=getPhysicalInterfaces())
+
+
+
+
+
+
