2a87310bc60a9dc804603088906b7f609980cfdf
Matt Oswalt <matt@keepingitclassless.net>
2014-05-25 20:43:52 -0400

    Simplified config method in templates
    Now passing a "config" dictionary into every template, without exception. This may be just a single key/value, and the value could be anything (list, another dict, etc.) The point is to predictably pass the same thing to the template each time. Each piece of dynamic data inside any template will have to refer to some node within the "config" dictionary.

--- methods.py
+++ methods.py
@@ -17,20 +17,7 @@ def get_config(file):
 def gen_snippet(snippet, config):
     '''Renders a config snippet. "config" represents the portion of the YAML file applicable to this snippet.'''
     template = env.get_template(snippet)
-    #See if you can simplify the below even more - may just accept a standard "config" dictionary in each Jinja template so you can refer to it the same way each time
-    #See "misc" snippet
-    if snippet == 'features':
-        return template.render(features=config)
-    elif snippet == 'vlans':
-        return template.render(vlans=config)
-    elif snippet == 'fcalias':
-        return template.render(initDict=config['initiators'], targetDict=config['targets'])
-    elif snippet == 'qos':
-        return template.render(qos=config)
-    elif snippet == 'ports':
-        return template.render(ports=config)
-    elif snippet == 'misc':
-        return template.render(config=config)
+    return template.render(config=config)
 
 def getucswwpns(module):
     #Need UCS Python SDK for this
