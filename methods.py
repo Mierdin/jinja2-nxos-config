b6f5676eacebc8e394bbd84e9ad9169209fa03cd
Matt Oswalt <matt@keepingitclassless.net>
2014-06-04 13:06:05 -0700

    Cleaned up a few things, re-edited YAML config file in VIM

--- methods.py
+++ methods.py
@@ -5,21 +5,22 @@
     and generates a Jinja2 configuration
 """
 
-#TODO: Clean up unused imports
 #TODO: Consider use of the leading underscore
 #from ucstools import storage
-from jinja2 import Template, Environment, FileSystemLoader
-from UcsSdk import *
-import yaml, os
+from jinja2 import Environment, FileSystemLoader
+import yaml
 
 ENV = Environment(loader=FileSystemLoader('./snippets/'))
 
 def get_config(configfile):
     """Pulls YAML configuration from file and returns dict object"""
-    with open(configfile) as f:
-        return yaml.load(f)
+    with open(configfile) as _:
+        return yaml.load(_)
 
 def gen_snippet(snippet, config):
-    """Renders a config snippet. "config" represents the portion of the YAML file applicable to this snippet"""
+    """Renders a config snippet.
+    "config" represents the portion of the YAML
+    file applicable to this snippet"""
+
     template = ENV.get_template(snippet)
     return template.render(config=config)
